6f7b4ddd4d45395817289e3b465bad85494c3e80
quaxsze <service@plum.eu.com>
2020-04-08 20:42:42 +0200

    fix signal

--- udata_metrics/__init__.py
+++ udata_metrics/__init__.py
@@ -4,7 +4,6 @@ udata-metrics
 Connexion handler to metrics service for udata
 '''
 
-from flask import _app_ctx_stack
 
 from udata_metrics.client import metrics_client_factory
 
--- udata_metrics/tasks.py
+++ udata_metrics/tasks.py
@@ -3,14 +3,14 @@ import logging
 from udata_metrics.client import InfluxClient
 from udata.models import Dataset
 from udata.tasks import job, task
-from udata.core.metrics.signals import on_site_metrics
+from udata.core.metrics.signals import on_site_metrics_computed
 
 from udata_metrics.client import metrics_client_factory
 
 log = logging.getLogger(__name__)
 
 
-@on_site_metrics.connect
+@on_site_metrics_computed.connect
 def site_metrics_send(document, **kwargs):
     write_object_metrics.delay(document)
 
