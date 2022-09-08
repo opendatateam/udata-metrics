c464855bf48f8c21783c72d46859bd6debf495d1
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2022-09-08 09:48:17 +0200

    WIP update to build metrics from Vector instead of udata-piwik (Matomo)

--- udata_metrics/client.py
+++ udata_metrics/client.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from flask import current_app
 from influxdb import InfluxDBClient
 
@@ -19,6 +20,31 @@ class InfluxClient:
 
     def insert(self, body):
         self.client.write_points([body])
+
+    def aggregate_metrics(self, measurement: str):
+        """measurement one of reuse.reuse_id, resource.resource_id,
+        dataset.dataset_id, organization.organization_id,
+        resource_hit.resource_id"""
+        dt = datetime.now()
+        id_key = f"{measurement}".split(".")[1]
+        # TODO: Replace with right timezone for logs in the query
+        query = f"""
+                from(bucket: "vector-bucket")
+                    |> range(start: {dt.strftime("%Y-%m-%d") - timedelta(days=1)}T00:00:00.00Z, end: {dt.strftime("%Y-%m-%d")}T00:00:00.00Z)
+                    |> filter(fn: (r) => r._measurement == "{measurement}")
+                    |> map(fn: (r) => ({{r with day: r._time.strftime("%Y-%m-%d")}}))
+                    |> group(columns: ["{id_key}"])
+                    |> count()
+                    |> map(fn: (r) => ({{r with _time: r._start, _field: "resource", _measurement: "count"}}))
+                    |> to(
+                        bucket: "{current_app.config["METRICS_INFLUX"]['INFLUX_BUCKET']}",
+                        host: "{current_app.config["METRICS_INFLUX"]['INFLUX_URL']}",
+                        org: "{current_app.config["METRICS_INFLUX"]['INFLUX_ORG']}",
+                        token: "{current_app.config["METRICS_INFLUX"]['INFLUX_TOKEN']}"
+                    )
+                """
+        self.client.query_api().query(query)
+        # TODO: Delete unaggregated data for the day
 
 
 def metrics_client_factory():
--- udata_metrics/tasks.py
+++ udata_metrics/tasks.py
@@ -1,29 +1,18 @@
 import logging
-import datetime
-
-from udata_metrics.client import InfluxClient
-from udata.models import Dataset
-from udata.tasks import job, task
-from udata.core.metrics.signals import on_site_metrics_computed
 
 from udata_metrics.client import metrics_client_factory
 
 log = logging.getLogger(__name__)
 
 
-@on_site_metrics_computed.connect
-def site_metrics_send(document, **kwargs):
-    write_object_metrics.delay(document)
-
-
-@task
-def write_object_metrics(document):
-    dt = datetime.datetime.now()
-    client = metrics_client_factory()
-    metrics = document.get_metrics()
-    body = {
-        'time': dt,
-        'measurement': 'site_metrics',
-        'fields': metrics
+def aggregate_last_day():
+    measurements_dict = {
+        "reuse": "reuse.reuse_id",
+        "resource": "resource.resource_id",
+        "dataset": "dataset.dataset_id",
+        "organization": "organization.organization_id",
+        "resource_hit": "resource_hit.resource_id",
     }
-    client.insert(body)
+    for _, measurement in measurements_dict.items():
+        client = metrics_client_factory()
+        client.aggregate_metrics(measurement)
