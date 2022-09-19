c7255bf5dcd865de813daffd8417a660376c73cf
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2022-09-19 17:13:28 +0200

    fix dependency issue and update Influx aggregation query

--- udata_metrics/client.py
+++ udata_metrics/client.py
@@ -13,31 +13,36 @@ class InfluxClient:
         dataset.dataset_id, organization.organization_id,
         resource_hit.resource_id"""
         dt = datetime.now()
+        page_type = f"{measurement}".split(".")[0]
         id_key = f"{measurement}".split(".")[1]
         # TODO: Replace with right timezone for logs in the query
         query = f"""
+                import "date"
                 from(bucket: "vector-bucket")
-                    |> range(start: {dt.strftime("%Y-%m-%d") - timedelta(days=1)}T00:00:00.00Z, stop: {dt.strftime("%Y-%m-%d")}T00:00:00.00Z)
+                    |> range(start: 2022-06-02T00:00:00.000Z, stop: 2022-06-03T00:00:00.000Z)
                     |> filter(fn: (r) => r._measurement == "{measurement}")
-                    |> map(fn: (r) => ({{r with day: r._time.strftime("%Y-%m-%d")}}))
-                    |> group(columns: ["{id_key}"])
+                    |> map(fn: (r) => ({{r with day: date.truncate(t: r._time, unit: 1d)}}))
+                    |> group(columns: ["{id_key}", "day"])
                     |> count()
-                    |> map(fn: (r) => ({{r with _time: r._start, _field: "resource", _measurement: "count"}}))
+                    |> duplicate(column: "day", as: "_time")
+                    |> map(fn: (r) => ({{_field: "{page_type}", _measurement: "count"}}))
                     |> to(
                         bucket: "{current_app.config['METRICS_AGGREGATION_BUCKET']}",
                         host: "{current_app.config['METRICS_INFLUX_DSN']['url']}",
                         org: "{current_app.config['METRICS_INFLUX_DSN']['org']}",
-                        token: "{current_app.config['METRICS_INFLUX_DSN']['token']}"
+                        token: "{current_app.config['METRICS_INFLUX_DSN']['token']}",
+                        timeColumn: "_time"
                     )
                 """
+        print(query)
         self.client.query_api().query(query)
         # TODO: Ensure deletion works as intended
-        self.client.delete_api().delete(
-            start=f"{dt.strftime('%Y-%m-%d') - timedelta(days=1)}T00:00:00.00Z",
-            stop=f"{dt.strftime('%Y-%m-%d')}T00:00:00.00Z",
-            bucket=current_app.config['METRICS_VECTOR_BUCKET'],
-            org=current_app.config['METRICS_INFLUX_DSN']['org']
-        )
+        # self.client.delete_api().delete(
+        #     start=f"{dt.strftime('%Y-%m-%d') - timedelta(days=1)}T00:00:00.00Z",
+        #     stop=f"{dt.strftime('%Y-%m-%d')}T00:00:00.00Z",
+        #     bucket=current_app.config['METRICS_VECTOR_BUCKET'],
+        #     org=current_app.config['METRICS_INFLUX_DSN']['org']
+        # )
 
 
 def metrics_client_factory():
