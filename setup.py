382f1f9c871275445365601440059615999ab248
Thibaud Dauce <git@hadibut.fr>
2025-07-02 14:42:07 +0200

    Remove entrypoint

--- setup.py
+++ setup.py
@@ -6,77 +6,72 @@ import re
 
 from setuptools import setup
 
-RE_BADGE = re.compile(r'^\[\!\[(?P<text>.*?)\]\[(?P<badge>.*?)\]\]\[(?P<target>.*?)\]$', re.M)
+RE_BADGE = re.compile(
+    r"^\[\!\[(?P<text>.*?)\]\[(?P<badge>.*?)\]\]\[(?P<target>.*?)\]$", re.M
+)
 
 BADGES_TO_KEEP = []
 
 
 def md(filename):
-    '''
+    """
     Load .md (markdown) file and sanitize it for PyPI.
-    '''
+    """
     content = io.open(filename).read()
 
     for match in RE_BADGE.finditer(content):
-        if match.group('badge') not in BADGES_TO_KEEP:
-            content = content.replace(match.group(0), '')
+        if match.group("badge") not in BADGES_TO_KEEP:
+            content = content.replace(match.group(0), "")
 
     return content
 
 
 def pip(filename):
     """Parse pip reqs file and transform it to setuptools requirements."""
-    return open(os.path.join('requirements', filename)).readlines()
+    return open(os.path.join("requirements", filename)).readlines()
 
 
-long_description = '\n'.join((
-    md('README.md'),
-    md('CHANGELOG.md'),
-    ''
-))
+long_description = "\n".join((md("README.md"), md("CHANGELOG.md"), ""))
 
-install_requires = pip('install.pip')
-tests_require = pip('test.pip')
+install_requires = pip("install.pip")
+tests_require = pip("test.pip")
 
 
 setup(
-    name='udata-metrics',
-    version=__import__('udata_metrics').__version__,
-    description=__import__('udata_metrics').__description__,
+    name="udata-metrics",
+    version=__import__("udata_metrics").__version__,
+    description=__import__("udata_metrics").__description__,
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/opendatateam/udata-metrics',
-    author='Open Data Team',
-    author_email='contact@opendata.team',
-    packages=['udata_metrics'],
+    long_description_content_type="text/markdown",
+    url="https://github.com/opendatateam/udata-metrics",
+    author="Open Data Team",
+    author_email="contact@opendata.team",
+    packages=["udata_metrics"],
     include_package_data=True,
-    python_requires='>=3.7',
+    python_requires=">=3.7",
     install_requires=install_requires,
     tests_require=tests_require,
     extras_require={
-        'test': tests_require,
+        "test": tests_require,
     },
     entry_points={
-        'udata.views': [
-            'metrics = udata_metrics.views',
-        ],
-        'udata.tasks': [
-            'metrics = udata_metrics.tasks',
+        "udata.tasks": [
+            "metrics = udata_metrics.tasks",
         ]
     },
-    license='AGPL',
+    license="AGPL",
     zip_safe=False,
-    keywords='udata metrics',
+    keywords="udata metrics",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python',
-        'Environment :: Web Environment',
-        'Operating System :: OS Independent',
-        'Intended Audience :: Developers',
-        'Topic :: System :: Software Distribution',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: System :: Software Distribution",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
